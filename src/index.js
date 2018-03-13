4361c41d79f0434a4fa29858d3cb06326d197272
Tim Carleton <carletontimothy@gmail.com>
2018-03-13 16:34:32 -0400

    map displaying

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("This is running");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiY2FybGV0MG4iLCJhIjoiY2plcTNuanAwMGhtZjMzcW5hb28xZ2FidyJ9.ixMknuS_cVGym6QQwsgYsQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
